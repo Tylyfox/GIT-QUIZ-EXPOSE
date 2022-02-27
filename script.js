d1953c6e2e7d1beafca3a351a4ef23866c8d749b
Valentin Cadart <cadart.valentin@sfr.fr>
2022-02-27 22:35:13 +0100

    Ajout de l'index.html, du css et javacsript

---
+++ script.js
@@ -0,0 +1,31 @@
+ //objet questionnaire
+
+
+
+ //définition des variables et constantes
+
+
+//fonctions
+
+    //fonction qui lance le jeu quand on clique sur le bonton Commencer le quizz
+    
+
+    //fonction qui affiche la question de l'objet
+   
+
+//fonction qui affiche le bouton suivant et la réponse après validation 
+
+   
+
+
+
+//fontion qui change la couleur du bouton lorsque l'on le sélectionne
+
+
+//fonction qui reinitailise les couleurs des boutons
+
+
+
+
+
+//évènements
